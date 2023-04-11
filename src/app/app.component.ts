e8006e809402f8f20abb47815f01e826c08bf1ec
Thien Tran <tranngocthien2609@gmail.com>
2023-04-11 19:09:40 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface SaleData {
+  salesDate: any;
+  storeName: string;
+  productName: string;
+  salesUnit: number;
+  salesRevenue: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'sale-management';
+  dataSource = new MatTableDataSource<SaleData>();
+  displayedColumns: string[] = ['salesDate', 'storeName', 'productName', 'salesUnit', 'salesRevenue'];
+
+  ngOnInit() {
+    this.initializeWebSocketConnection();
+  }
+
+  public stompClient: any;
+
+  initializeWebSocketConnection() {
+    const serverUrl = 'http://localhost:8081/vnet-websocket';
+    const ws = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect({}, (frame : any) => {
+      this.stompClient.subscribe('/vnet', (message : any) => {
+        console.log(message);
+        if (message.body) {
+          this.dataSource.data = JSON.parse(message.body);
+        }
+      });
+    });
+  }
+}
