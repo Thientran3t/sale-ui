fc3c2287994df0371e94d588b2af88cc20423b43
Thien Tran <tranngocthien2609@gmail.com>
2023-04-12 22:22:05 +0700

    call API to trigger fetch data, pagination table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import { MatTableDataSource } from '@angular/material/table';
-
+import { MatPaginator } from '@angular/material/paginator';
+import { ApiService } from './api.service';
 export interface SaleData {
   salesDate: any;
   storeName: string;
@@ -17,13 +18,24 @@ export interface SaleData {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  constructor(private appService: ApiService) { };
+
   title = 'sale-management';
   dataSource = new MatTableDataSource<SaleData>();
   displayedColumns: string[] = ['salesDate', 'storeName', 'productName', 'salesUnit', 'salesRevenue'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit() {
     this.initializeWebSocketConnection();
+    this.appService.callApiTest().subscribe(() => {
+      console.log("Trigger fetch data");
+    })
   }
 
   public stompClient: any;
@@ -32,9 +44,8 @@ export class AppComponent {
     const serverUrl = 'http://localhost:8081/vnet-websocket';
     const ws = new SockJS(serverUrl);
     this.stompClient = Stomp.over(ws);
-    this.stompClient.connect({}, (frame : any) => {
-      this.stompClient.subscribe('/vnet', (message : any) => {
-        console.log(message);
+    this.stompClient.connect({}, (frame: any) => {
+      this.stompClient.subscribe('/vnet', (message: any) => {
         if (message.body) {
           this.dataSource.data = this.dataSource.data.concat(JSON.parse(message.body));
         }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
